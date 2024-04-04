20f6582f57c85c8bcd228762277c4ab74f02a793
Steve Potter <stephen@redefinesurgery.com>
2024-04-04 16:07:24 -0400

    Initial Commit

---
+++ RedefineGuide/RedefineGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedefineGuideApp.swift
+//  RedefineGuide
+//
+//  Created by Stephen Potter on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RedefineGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
